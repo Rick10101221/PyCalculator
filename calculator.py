11f01f6489515130151216f81cd55a5362efa2ad
Rickesh Khilnani <rickysbest@gmail.com>
2020-09-06 00:15:34 -0700

    Understanding PyQt5

--- calculator.py
+++ calculator.py
@@ -1,11 +1,36 @@
 print('Python code starting...')
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel,\
+    QMessageBox
+from PyQt5.QtCore import Qt
 
-app = QApplication(sys.argv)
+def dialog():
+    mbox = QMessageBox()
+    
+    mbox.setText("Your allegiance has been noted")
+    mbox.setDetailedText("You are now a fucking idiot")
+    mbox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
 
-window = QWidget()
-window.show()
+    mbox.exec_()
 
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(300, 300)
+    w.setWindowTitle('calculator')
+
+    label = QLabel(w)
+    label.setText("Hello")
+    #label.setAlignment(Qt.AlignCenter)
+    label.move(110, 130)
+    label.show()
+
+    btn = QPushButton(w)
+    btn.setText('Beheld')
+    btn.move(110, 150)
+    btn.show()
+    btn.clicked.connect(dialog)
+
+    w.show()
+    app.exec_()
